c72483e7e25c20a610ca6a647d22bf88306ea98b
Kevin <kevin@cateina.com>
2021-11-10 23:48:38 +0530

    added ping route

--- api/routes/index.js
+++ api/routes/index.js
@@ -4,6 +4,10 @@ const apiRoutes = express.Router();
 const dmartRoutes = require('./dmart.route');
 const jiomartRoutes = require('./jiomart.route');
 
+apiRoutes.use('/api/ping', function(res, res) {
+    console.log('Ping!');
+    res.send('Ping!');
+})
 apiRoutes.use('/api/scrape/dmartProducts', dmartRoutes);
 apiRoutes.use('/api/scrape/jiomartProducts', jiomartRoutes);
 
