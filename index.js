99fa990f3d58d0d05ebea337326808701a3c369d
Kevin <kevin@cateina.com>
2021-11-26 00:12:38 +0530

    add code to handle cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
 
 const express = require('express');
+const cors = require('cors');
 const app = express();
 
 const apiRoutes = require('./api/routes/index');
 
+app.use(cors());
 app.use(express.json());
 
 app.use(apiRoutes);
